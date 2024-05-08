6297b5397415704a6282d45b0ca6f206b9788a98
Cirrus <78839598+CirrusNeptune@users.noreply.github.com>
2024-05-07 19:39:36 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -11,24 +11,8 @@
             # of this flake.
             system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
 
+            # Use configuration.nix for everything
             imports = [ ./configuration.nix ];
-
-            # Enable flakes
-            nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-            # Use the systemd-boot EFI boot loader.
-            boot.loader.systemd-boot.enable = true;
-            boot.loader.efi.canTouchEfiVariables = true;
-
-            # Network configuration.
-            networking.useDHCP = false;
-            networking.firewall.allowedTCPPorts = [ 80 ];
-
-            # Enable a web server.
-            services.httpd = {
-              enable = true;
-              adminAddr = "morty@example.org";
-            };
           })
         ];
     };
