b782c5933fc81196f47bfca3bf92921e23656bfc
CirrusNeptune <cirrus.neptune@protonmail.com>
2024-05-12 11:52:31 -0700

    Add user a

--- configuration.nix
+++ configuration.nix
@@ -144,14 +144,14 @@ in
   # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     firefox
-  #     tree
-  #   ];
-  # };
+  users.users.a = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    #packages = with pkgs; [
+    #  firefox
+    #  tree
+    #];
+  };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
