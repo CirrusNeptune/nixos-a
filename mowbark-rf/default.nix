ca5790e711b2922916bd074cd376a4b713a77e4e
CirrusNeptune <cirrus.neptune@protonmail.com>
2024-05-18 13:52:33 -0700

    asdf

--- mowbark-rf/default.nix
+++ mowbark-rf/default.nix
@@ -1,14 +1,14 @@
-{ lib, pkgs, symlinkJoin, ... }:
+{ lib, pkgs, fetchFromGithub, symlinkJoin, ... }:
 let
   src = pkgs.symlinkJoin {
     name = "mowbark-rf-workspace";
     paths = [
-      lib.fetchFromGithub {
+      fetchFromGithub {
         owner = "CirrusNeptune";
         repo = "libftd2xx-cc1101";
         rev = "3ba5aaa4bda7af31a31850eb0ec6d5101b593f34";
       }
-      lib.fetchFromGithub {
+      fetchFromGithub {
         owner = "CirrusNeptune";
         repo = "nexus-revo-io";
         rev = "0feaa6052d3429c6ddfc1727bd157d5bbc8aa731";
@@ -17,8 +17,7 @@ let
   };
   #crate2nixTools = lib.crate2nix.tools { inherit lib pkgs; };
   #mowbark-rf = pkgs.callPackage ./Cargo.nix { inherit pkgs; };
-in
-rec {
+in {
   #mowbark-rf = crate2nixTools.appliedCargoNix {
   #  name = "mowbark-rf";
   #  src = ./.;
