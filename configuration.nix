77fd95df9d0ba232240c54ee5dda9431452f57ab
CirrusNeptune <cirrus.neptune@protonmail.com>
2024-05-18 15:00:54 -0700

    asdf

--- configuration.nix
+++ configuration.nix
@@ -228,6 +228,7 @@ in
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    git
     mowbark-rf
   ];
 
