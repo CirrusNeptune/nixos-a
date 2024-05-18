d1091a590a902963edf217465e77916b8d4b1ab9
CirrusNeptune <cirrus.neptune@protonmail.com>
2024-05-18 11:59:12 -0700

    asdf

--- mowbark-rf/default.nix
+++ mowbark-rf/default.nix
@@ -1,10 +1,11 @@
 { lib, pkgs, ... }:
-#let
-#  mowbark-rf = pkgs.callPackage ./Cargo.nix { inherit pkgs; };
-#in
+let
+  crate2nix_tools = lib.crate2nix.tools { inherit lib pkgs; };
+  #mowbark-rf = pkgs.callPackage ./Cargo.nix { inherit pkgs; };
+in
 {
   #mowbark-rf = import ./Cargo.nix { inherit pkgs; };
-  blah = lib.trace lib.crate2nix {};
+  blah = lib.trace crate2nix_tools {};
 }
 #{
   #systemd.services.mowbark-rf = {
