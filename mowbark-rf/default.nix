bab361fe3d7feee726dfee2abac5197dbf7ad8a7
CirrusNeptune <cirrus.neptune@protonmail.com>
2024-05-18 14:00:05 -0700

    saf

--- mowbark-rf/default.nix
+++ mowbark-rf/default.nix
@@ -1,8 +1,5 @@
-{ lib, pkgs, fetchFromGithub, symlinkJoin, rustPlatform, ... }:
-let
-  #crate2nixTools = lib.crate2nix.tools { inherit lib pkgs; };
-  #mowbark-rf = pkgs.callPackage ./Cargo.nix { inherit pkgs; };
-in rustPlatform.buildRustPackage {
+{ lib, fetchFromGithub, symlinkJoin, rustPlatform, ... }:
+rustPlatform.buildRustPackage {
   pname = "mowbark-rf";
   version = "0.0.1";
 
@@ -11,6 +8,21 @@ in rustPlatform.buildRustPackage {
     repo = "nexus-revo-io";
     rev = "0feaa6052d3429c6ddfc1727bd157d5bbc8aa731";
   };
+  #src = symlinkJoin {
+  #  name = "mowbark-rf-workspace";
+  #  paths = [
+  #    fetchFromGithub {
+  #      owner = "CirrusNeptune";
+  #      repo = "libftd2xx-cc1101";
+  #      rev = "3ba5aaa4bda7af31a31850eb0ec6d5101b593f34";
+  #    }
+  #    fetchFromGithub {
+  #      owner = "CirrusNeptune";
+  #      repo = "nexus-revo-io";
+  #      rev = "0feaa6052d3429c6ddfc1727bd157d5bbc8aa731";
+  #    }
+  #  ];
+  #};
 }
 #{
   #systemd.services.mowbark-rf = {
