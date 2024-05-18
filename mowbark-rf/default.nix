e2a3340d86675b784279bac194bc79a7efbee0af
CirrusNeptune <cirrus.neptune@protonmail.com>
2024-05-18 14:57:48 -0700

    asdf

--- mowbark-rf/default.nix
+++ mowbark-rf/default.nix
@@ -12,7 +12,7 @@ rustPlatform.buildRustPackage {
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes = {
-      "libftd2xx-cc1101-0.1.0" = lib.fakeHash;
+      "libftd2xx-cc1101-0.1.0" = "sha256-Vz/YMNSK3ay0xTVT3ic6d4/h61IDl2yUUJ17lsHpoNU=";
     };
   };
 }
