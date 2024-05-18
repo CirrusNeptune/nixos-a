50ce40d5940495825436972f7f090c7762e6dc9e
CirrusNeptune <cirrus.neptune@protonmail.com>
2024-05-18 13:58:30 -0700

    sdf

--- mowbark-rf/default.nix
+++ mowbark-rf/default.nix
@@ -2,7 +2,16 @@
 let
   #crate2nixTools = lib.crate2nix.tools { inherit lib pkgs; };
   #mowbark-rf = pkgs.callPackage ./Cargo.nix { inherit pkgs; };
-in {}
+in rustPlatform.buildRustPackage {
+  pname = "mowbark-rf";
+  version = "0.0.1";
+
+  src = fetchFromGithub {
+    owner = "CirrusNeptune";
+    repo = "nexus-revo-io";
+    rev = "0feaa6052d3429c6ddfc1727bd157d5bbc8aa731";
+  };
+}
 #{
   #systemd.services.mowbark-rf = {
   #  description = "Mowbark RF";
