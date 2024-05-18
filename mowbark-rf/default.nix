e6e2d9810bdae000bb07a767f2dc55dc61d3039e
CirrusNeptune <cirrus.neptune@protonmail.com>
2024-05-18 11:20:06 -0700

    tools

--- mowbark-rf/default.nix
+++ mowbark-rf/default.nix
@@ -4,7 +4,7 @@
 #in
 {
   #mowbark-rf = import ./Cargo.nix { inherit pkgs; };
-  blah = lib.trace pkgs.crate2nix {};
+  blah = lib.trace pkgs.crate2nix.tools {};
 }
 #{
   #systemd.services.mowbark-rf = {
