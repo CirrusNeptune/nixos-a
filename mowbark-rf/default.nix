633b2d0594efed6896b33da48b63a5513fcf50bb
CirrusNeptune <cirrus.neptune@protonmail.com>
2024-05-18 10:46:48 -0700

    Disable test

--- mowbark-rf/default.nix
+++ mowbark-rf/default.nix
@@ -1,13 +1,13 @@
 { lib, pkgs, ... }:
 let
-  mowbark-rf = pkgs.callPackage ./Cargo.nix {};
+  #mowbark-rf = pkgs.callPackage ./Cargo.nix {};
 in {
-  systemd.services.mowbark-rf = {
-    description = "Mowbark RF";
-    wantedBy = [ "podman-homeassistant.service" ];
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${lib.getBin mowbark-rf}/bin/nexus-revo-io";
-    };
-  };
+  #systemd.services.mowbark-rf = {
+  #  description = "Mowbark RF";
+  #  wantedBy = [ "podman-homeassistant.service" ];
+  #  serviceConfig = {
+  #    Type = "simple";
+  #    ExecStart = "${lib.getBin mowbark-rf}/bin/nexus-revo-io";
+  #  };
+  #};
 }
