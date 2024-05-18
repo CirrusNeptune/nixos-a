cf56e1e824f9855e5f30d710c7a3b7e6906052a5
CirrusNeptune <cirrus.neptune@protonmail.com>
2024-05-18 13:46:21 -0700

    asdf

--- mowbark-rf/default.nix
+++ mowbark-rf/default.nix
@@ -1,14 +1,29 @@
 { lib, pkgs, ... }:
 let
-  crate2nixTools = lib.crate2nix.tools { inherit lib pkgs; };
+  src = lib.symlinkJoin {
+    name = "mowbark-rf-workspace";
+    paths = [
+      lib.fetchFromGithub {
+        owner = "CirrusNeptune";
+        repo = "libftd2xx-cc1101";
+        rev = "3ba5aaa4bda7af31a31850eb0ec6d5101b593f34";
+      }
+      lib.fetchFromGithub {
+        owner = "CirrusNeptune";
+        repo = "nexus-revo-io";
+        rev = "0feaa6052d3429c6ddfc1727bd157d5bbc8aa731";
+      }
+    ];
+  };
+  #crate2nixTools = lib.crate2nix.tools { inherit lib pkgs; };
   #mowbark-rf = pkgs.callPackage ./Cargo.nix { inherit pkgs; };
 in
 rec {
-  mowbark-rf = crate2nixTools.appliedCargoNix {
-    name = "mowbark-rf";
-    src = ./.;
-  };
-  blah = lib.trace mowbark-rf {};
+  #mowbark-rf = crate2nixTools.appliedCargoNix {
+  #  name = "mowbark-rf";
+  #  src = ./.;
+  #};
+  blah = lib.trace src {};
 }
 #{
   #systemd.services.mowbark-rf = {
