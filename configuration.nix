0ba009380fc0ef2cf8af2e87a8a16c88e687c662
CirrusNeptune <cirrus.neptune@protonmail.com>
2024-05-19 14:20:58 -0700

    Add gdb as system package

--- configuration.nix
+++ configuration.nix
@@ -127,6 +127,7 @@ in
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
+    gdb
   ];
 
   # Add docker containers
