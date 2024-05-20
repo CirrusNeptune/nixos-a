9a80ba6e91815c169b5e3db377e4898e89ce6846
CirrusNeptune <cirrus.neptune@protonmail.com>
2024-05-19 18:12:20 -0700

    asdf

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,6 @@
 pkgs: with pkgs; {
   mowbark-rf = callPackage ./mowbark-rf {};
-  dockerToolsLocal = callPackage ./docker {};
+  dockerToolsLocal = callPackage ./docker {
+    writePython3 = buildPackages.writers.writePython3;
+  } // { __attrsFailEvaluation = true; };
 }
