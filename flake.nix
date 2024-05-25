b20fdd962bed3e1c25dedec58688c29a1e4800ad
CirrusNeptune <cirrus.neptune@protonmail.com>
2024-05-25 15:38:24 -0700

    Attempt to build kernel with gcc14

--- flake.nix
+++ flake.nix
@@ -22,6 +22,7 @@
 
           # Package overlays
           nixpkgs.overlays = [
+            (final: prev: { linuxKernel = prev.linuxKernel.override { stdenv = prev.gcc14Stdenv; }; })
             (final: prev: import ./pkgs final)
           ];
 
