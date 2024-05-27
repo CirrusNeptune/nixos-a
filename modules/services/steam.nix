9590a3fb3cae6326e8c960356286915b3423101f
CirrusNeptune <cirrus.neptune@protonmail.com>
2024-05-27 13:46:41 -0700

    asdf

--- modules/services/steam.nix
+++ modules/services/steam.nix
@@ -10,13 +10,26 @@ in {
     };
   };
 
-  config = lib.mkIf cfg.enable (lib.a.makeGamescopeService {
-    inherit config lib pkgs;
-    service = "steam";
-    tty = "tty3";
-    user = cfg.user;
-    program = "${lib.getBin pkgs.steam}/bin/steam";
-    args = [ "-tenfoot" "-pipewire-dmabuf" ];
-    gamescopeArguments = [ "--steam" ];
-  });
+  config = lib.mkIf cfg.enable (lib.mkMerge [
+    (lib.a.makeGamescopeService {
+      inherit config lib pkgs;
+      service = "steam";
+      tty = "tty3";
+      user = cfg.user;
+      program = "${lib.getBin pkgs.steam}/bin/steam";
+      args = [ "-tenfoot" "-pipewire-dmabuf" ];
+      gamescopeArguments = [ "--steam" ];
+    })
+    {
+      security.wrappers = {
+        # needed or steam fails
+        bwrap = {
+          owner = "root";
+          group = "root";
+          source = "${pkgs.bubblewrap}/bin/bwrap";
+          setuid = true;
+        };
+      };
+    }
+  ]);
 }
