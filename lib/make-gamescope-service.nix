18342dd9a7b47a9e2d4117e449e6a14c6ec38255
CirrusNeptune <cirrus.neptune@protonmail.com>
2024-05-31 21:14:00 -0700

    PAM is dumb

--- lib/make-gamescope-service.nix
+++ lib/make-gamescope-service.nix
@@ -71,7 +71,7 @@
     account required pam_unix.so
     session required pam_unix.so
     session required pam_env.so conffile=/etc/pam/environment readenv=0
-    session required ${config.systemd.package}/lib/security/pam_systemd.so default-capability-ambient-set=
+    session required ${config.systemd.package}/lib/security/pam_systemd.so default-capability-ambient-set=~0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63
   '';
 
   systemd.targets.graphical.wants = [ "${service}.service" ];
