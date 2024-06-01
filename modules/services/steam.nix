4a03e7c89720b5231408a8c431793dee285dac04
CirrusNeptune <cirrus.neptune@protonmail.com>
2024-05-31 19:51:37 -0700

    Steam dump test

--- modules/services/steam.nix
+++ modules/services/steam.nix
@@ -16,9 +16,9 @@ in {
       service = "steam";
       tty = "tty3";
       user = cfg.user;
-      program = "${lib.getBin pkgs.coreutils}/bin/env";
+      program = "${lib.getBin pkgs.bash}/bin/bash -c \"${lib.getBin pkgs.steam}/bin/steam -tenfoot -pipewire-dmabuf > /home/a/mowsteam.txt\"";
       args = [];
-      gamescopeArguments = [];
+      gamescopeArguments = [ "--steam" ];
     })
     {
       programs.steam = {
