56d023f52318e1da115472fe31661c0896d2c0bc
Poofy <poofy.enigma@gmail.com>
2026-03-08 20:45:52 -0700

    Flatpak and python

--- configuration.nix
+++ configuration.nix
@@ -229,6 +229,9 @@ in
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
+  # Flatpak support for building itch.io app
+  services.flatpak.enable = true;
+
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
@@ -237,6 +240,8 @@ in
     file
     claude-code
     jetbrains.pycharm-community
+    flatpak-builder
+    python3
   ];
 
   # Add docker containers
